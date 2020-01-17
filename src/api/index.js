f9e65262e1a7f4bb0f5c86e439d0a49ca61d7e80
Mir Imad Ahmed <mirimadahmed@outlook.com>
2020-01-17 11:16:19 +0500

    api scene added

---
+++ src/api/index.js
@@ -0,0 +1,20 @@
+import axios from 'axios'
+
+const axiosObj = axios.create({
+  baseURL: 'https://bookable.pk/nic'
+})
+
+export default {
+  users () {
+    return axiosObj.get(`/users.php`)
+  },
+  startups () {
+    return axiosObj.get(`/startups.php`)
+  },
+  techs () {
+    return axiosObj.get(`/techs.php`)
+  },
+  industries () {
+    return axiosObj.get(`/industries.php`)
+  }
+}
