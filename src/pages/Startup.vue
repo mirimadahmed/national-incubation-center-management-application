9438eaa810d245e3207d26f78794258570c59836
Mir Imad Ahmed <mirimadahmed@outlook.com>
2020-01-17 13:56:11 +0500

    new startup page

---
+++ src/pages/Startup.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="row">
+    <div class="col-md-12">
+      <startup-profile></startup-profile>
+    </div>
+  </div>
+</template>
+<script>
+import StartupProfile from "./UserProfile/StartupProfile.vue";
+export default {
+  components: {
+    StartupProfile
+  }
+};
+</script>
+<style>
+</style>
